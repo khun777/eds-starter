462fe1fb09070e8ca8f12cca5ca2485cb22d86b0
Ralph Schaer <ralphschaer@gmail.com>
2014-09-03 05:19:33 +0200

    Use nice icons for the navigation tree

--- src/main/java/ch/rasc/eds/starter/service/NavigationService.java
+++ src/main/java/ch/rasc/eds/starter/service/NavigationService.java
@@ -36,7 +36,7 @@ public class NavigationService {
 		businessNode
 				.addChild(new MenuNode(
 						"Dashboard",
-						"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAAA50lEQVR4Ab2Qr0vHUBTFxxjDP8AgpgWDGEQMBpNBFgxLYjSZxSSysCYiBoN5UeRz3wTBIMMoQ5aMYhgLhmEwyoKM5+UxvyD2ncE4755zf3oTogjZ4Yxr/c5JzNxfMeCIDktDyQNvyj45MeEoyyLPfHNllmSNnBuJTcQFPS8SOZmGRtaVrVBxwAcDib6WeeVdLVR0suApNCvT/z2W0lWep6X2OGRgfzT0VNiZIeabVAmXDBzf+driy8muBXv0mFvfjUnKwJNs/Q4pq4AlL4LZomxSY+l4pKR1C+/+O5VskLlDnbKtpafDD+DijjGiKOJHAAAAAElFTkSuQmCC",
+						"resources/images/eye.png",
 						"Starter.view.dummy.View", Role.USER));
 		root.addChild(businessNode);
 
@@ -45,7 +45,7 @@ public class NavigationService {
 		administrationNode
 				.addChild(new MenuNode(
 						"navigation_administration_users",
-						"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAAArklEQVR4Ac2NMQ6CQBREf2HBAaioiScgnIJYcABiOAgXoaKcDx2FMdpScAQOYYyFsSAbnd3faKxNnCn2ZWb4yL8Je9zo6qvQDGUfcXDFk76I9BFKzcSEHI5xS3qEwZ3U8nXIbVCFeCYdAx1IcyD7mabhQkcqfKwFBx3JaSomTFg0GWI04btmiDXBgsnKGic47DD6O2bSyMSxqQWrj4atVea3ZJXzxlvsNX8k8nu9AB/XjIWC18JAAAAAAElFTkSuQmCC",
+						"resources/images/users.png",
 						"Starter.view.user.Grid", Role.ADMIN));
 		root.addChild(administrationNode);
 
@@ -53,17 +53,17 @@ public class NavigationService {
 		systemNode
 				.addChild(new MenuNode(
 						"navigation_system_accesslog",
-						"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAA00lEQVQ4y2NYuXJlMxD/JRG/ZoABqAHPgTiBBByBbsBNKDsGiDNIwGboBjwG4v8k4GZsBuxcvXq1GgFsgM+A1SD2qlWrIoDsPHQMFDdav349D0EDgPR0IN6LBYcRZQDQmVpA28zQMVBcglgXHAXi71hwEVEG4APEumA/EH/EgvPWrl3LBzWgEV8sGACxDTIG+t9jzZo1GkA1ZVADokjyAlBuPVIieg40lItUA45CNYMylB9M0ASUaJAMuA3EE7FgmO0/gTgJlw0P8WThj1BDDJD1AAB30hUSZGmhygAAAABJRU5ErkJggg==",
+						"resources/images/data_scroll.png",
 						"Starter.view.accesslog.TabPanel", Role.ADMIN));
 		systemNode
 				.addChild(new MenuNode(
 						"navigation_system_logevents",
-						"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAA00lEQVQ4y2NYuXJlMxD/JRG/ZoABqAHPgTiBBByBbsBNKDsGiDNIwGboBjwG4v8k4GZsBuxcvXq1GgFsgM+A1SD2qlWrIoDsPHQMFDdav349D0EDgPR0IN6LBYcRZQDQmVpA28zQMVBcglgXHAXi71hwEVEG4APEumA/EH/EgvPWrl3LBzWgEV8sGACxDTIG+t9jzZo1GkA1ZVADokjyAlBuPVIieg40lItUA45CNYMylB9M0ASUaJAMuA3EE7FgmO0/gTgJlw0P8WThj1BDDJD1AAB30hUSZGmhygAAAABJRU5ErkJggg==",
+						"resources/images/data_scroll.png",
 						"Starter.view.loggingevent.Grid", Role.ADMIN));
 		systemNode
 				.addChild(new MenuNode(
 						"navigation_system_config",
-						"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAABF0lEQVR4AXXPIUuDURTG8cchY2GILI0xxDCGisG0ZJBhMJnEYBQRm4JVBIMYFg0isrBg+J+7l0WDGIYMP8GCwWCQIbIsY84ruzB4X9Tf4cI9l+ccuJqwAjs2p780U1aQuMRzPQ5GKSVxxYAuPlSXAfXfAR+vZGC87iuc46lFHYb7t+LohKmGAm5C99Sc1gTD8FRTwEXoRrGAy9PG825lyUr08HRcQWqlWxkp4DRMDXlmEG7nUdq2ueXBttysJO7xiWq7eTx93vBUJVmePSvRDsvL7FqRBt6KLkufR02wirM1q9qRLXOAt02r8ElNcbbBCxFdt8SI8BvWFccJ9WaaD1twOfY5s5ySrMIrPe6ijP7jZtyKyyrmB9IlukeQunFqAAAAAElFTkSuQmCC",
+						"resources/images/gearwheels.png",
 						"Starter.view.config.Form", Role.ADMIN));
 		root.addChild(systemNode);
 	}
