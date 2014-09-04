558c1691b32dd94103904f0f21f9dbe1cb1c397d
Ralph Schaer <ralphschaer@gmail.com>
2014-09-04 06:16:36 +0200

    Add application pid listener Makes it easier to find the pid for the shutdown script

--- src/main/java/ch/rasc/eds/starter/Application.java
+++ src/main/java/ch/rasc/eds/starter/Application.java
@@ -1,6 +1,7 @@
 package ch.rasc.eds.starter;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.system.ApplicationPidListener;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
@@ -24,7 +25,9 @@ public class Application extends SpringBootServletInitializer {
 
 	// -Dspring.profiles.active=development
 	public static void main(String[] args) throws Exception {
-		SpringApplication.run(Application.class, args);
+		SpringApplication app = new SpringApplication(Application.class);
+		app.addListeners(new ApplicationPidListener());		
+		app.run(args);
 	}
 
 	@Override
