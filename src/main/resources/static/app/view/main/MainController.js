a50540e292f2f7886d96ac67798f599dc8015c1d
Ralph Schaer <ralphschaer@gmail.com>
2014-09-06 20:37:13 +0200

    Add routes

--- src/main/resources/static/app/view/main/MainController.js
+++ src/main/resources/static/app/view/main/MainController.js
@@ -2,37 +2,27 @@ Ext.define('Starter.view.main.MainController', {
 	extend: 'Ext.app.ViewController',
 	requires: [ 'Starter.view.usersettings.Window' ],
 
-	init: function() {
-		securityService.getLoggedOnUser(this.afterLoggedOnUserReceived, this);
-	},
-
-	onNavigationStoreLoad: function(store) {
-		if (this.autoOpenView) {
-			var rec = store.findRecord('view', this.autoOpenView);
-			if (rec) {
-				this.onNavigationTreeSelectionchange(null, [rec]);
+	routes: {
+		':navigationId': {
+			action: 'onNavigationRoute',
+			conditions: {
+				':id': '([0-9]+)'
 			}
 		}
 	},
-	
-	afterLoggedOnUserReceived: function(user) {
-		this.getViewModel().set('loggedOnUser', user.firstName + ' ' + user.lastName);
-		this.autoOpenView = user.autoOpenView;
-	},
 
-	onNavigationTreeSelectionchange: function(tree, selections) {
-		if (selections && selections.length === 1) {
-			var selected = selections[0];
-			var view = selected.data.view;
-			var tabPanel = this.lookupReference('centerTabPanel');
-
+	onNavigationRoute: function(navigationId) {
+		var record = this.getStore('navigationStore').getById(navigationId);
+		if (record) {
+			var view = record.data.view;
 			if (view) {
-				var tab = tabPanel.child('panel[navigationId=' + selected.getId() + ']');
+				var tabPanel = this.lookupReference('centerTabPanel');
+				var tab = tabPanel.child('panel[navigationId=' + record.getId() + ']');
 				if (!tab) {
 					var viewObject = Ext.create(view, {
-						icon: selected.data.icon,
-						treePath: this.getPath(selected),
-						navigationId: selected.getId()
+						icon: record.data.icon,
+						treePath: this.getPath(record),
+						navigationId: record.getId()
 					});
 					tab = tabPanel.add(viewObject);
 				}
@@ -42,21 +32,51 @@ Ext.define('Starter.view.main.MainController', {
 		}
 	},
 
+	init: function() {
+		securityService.getLoggedOnUser(this.afterLoggedOnUserReceived, this);
+	},
+
+	onNavigationStoreLoad: function(store) {
+		if (this.autoOpenView) {
+			var record = store.findRecord('view', this.autoOpenView);
+			if (record) {
+				var navigationTree = this.lookupReference('navigationTree');
+				var path = this.getPath(record);
+				Ext.defer(function() {
+					navigationTree.selectPath(path);
+				}, 1);
+			}
+		}
+	},
+
+	afterLoggedOnUserReceived: function(user) {
+		this.getViewModel().set('loggedOnUser', user.firstName + ' ' + user.lastName);
+		this.autoOpenView = user.autoOpenView;
+	},
+
+	onNavigationTreeSelectionchange: function(tree, selections) {
+		if (selections && selections.length === 1) {
+			var selected = selections[0];
+			this.redirectTo('' + selected.getId(), true);
+		}
+	},
+
 	getPath: function(node) {
 		return node.parentNode ? this.getPath(node.parentNode) + "/" + node.getId() : "/" + node.getId();
 	},
 
 	onTabChange: function(tabPanel, newCard) {
+		//TODO: need a way to change the hash in the url
 		if (!this.activeTab || this.activeTab.getId() !== newCard.getId()) {
 			this.activeTab = newCard;
 			var navigationTree = this.lookupReference('navigationTree');
 			navigationTree.suspendEvents();
 			navigationTree.selectPath(newCard.treePath);
 			navigationTree.resumeEvents();
-		}		
+		}
 	},
 
-	onTabRemove: function(tabPanel) {		
+	onTabRemove: function(tabPanel) {
 		var navigationTree = this.lookupReference('navigationTree');
 		if (tabPanel.items.length === 0) {
 			navigationTree.getSelectionModel().deselectAll();
